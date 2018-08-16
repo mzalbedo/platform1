fd9964f15c7a90e3b71e65b56eb14487734f71cf
cannonwjl <1428943841@qq.com>
2018-08-16 11:42:01 +0800

    http package

--- utils/util.js
+++ utils/util.js
@@ -17,3 +17,43 @@ const formatNumber = n => {
 module.exports = {
   formatTime: formatTime
 }
+
+
+//---import /config.js under project of config and function of fun1
+//import {config ,fun1} from '/config.js'
+import { config } from '/config.js'
+class HTTP {
+  request(params) {
+    // url ,data, method
+    if (!params.method) {
+      params.method = "GET"
+    }
+    wx.request({
+      url: config.api_base_url + params.url,
+      method: params.method,
+      data: params.data,
+      header: {
+        'content-type': "application/json",
+        'appkey': config.appkey
+      },
+      success: (res) => {
+        //startsWith
+        //endsWith
+        let code = res.startsWith
+        if (code.startsWith('2')) {
+
+        } else {
+
+        }
+
+      },
+      fail: (err) => {
+
+      }
+    })
+
+
+
+  }
+}
+
